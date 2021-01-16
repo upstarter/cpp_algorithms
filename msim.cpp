73c394dd4705aab3ba9032c7de37d8d13adb7b25
Eric Steen <eric.steen@important.com>
2021-01-16 13:12:35 -0600

    msim

--- msim.cpp
+++ msim.cpp
@@ -11,13 +11,20 @@ int main(){
     double x,y;    // Coordinates
     int hit;       // storage variable of number of HITs
     srand(time(0));
-    for (int j=0;j<jmax;j++){
+    for (int j=0;j<jmax;j++) {
         hit=0;
         x=0; y=0;
+
         for(int i=0;i<imax;i++){
             x=double(rand())/double(RAND_MAX);
             y=double(rand())/double(RAND_MAX);
-        if(y<=sqrt(1-pow(x,2))) hit+=1; }          //Choosing HITs according to analytic formula of circle
-    cout<<""<<4*double(hit)/double(imax)<<endl; }  // Print out Pi number
+
+            //Choosing HITs according to analytic formula of circle
+            if(y<=sqrt(1-pow(x,2))) hit+=1;
+        }
+        
+        // Print out Pi number
+        cout<<""<<4*double(hit)/double(imax)<<endl;
+    }
 }
  
